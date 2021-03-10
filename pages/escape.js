b7e07e687f0f19800951cc54436a4a0637f1926b
Blake DeGraw <bbdegraw@gmail.com>
2021-03-10 11:49:30 -0800

    added escape details

--- pages/escape.js
+++ pages/escape.js
@@ -19,6 +19,13 @@ export default function Escape() {
             <Image src="/escape_screenshot_1.png" width={175} height={100}/>{' '}
             <Image src="/escape_screenshot_2.png" width={175} height={100}/>{' '}
             <Image src="/escape_screenshot_3.png" width={175} height={100}/>
+            <p>
+                <b>Escape from D-1471</b> is a short first-person shooter/platformer in which the player attempts to escape from a futuristic floating prison fortress. The prison is located in the upper atmosphere of a gas planet, and the player must escape to a gas-mining facility in orbit several miles below the prison. Because the planet's gravitational pull is significantly weaker than earth's, the prisoner attempts this escape by freefalling. 
+                <br></br><br></br>
+                In order to reoritent themselves along the way, the player must land on a series of solar platforms situated throughout the atmosphere. These platforms are comparatively small, and identifying them during freefall is designed to be immensely challenging. To make matters worse, sentry droids await the player on some of these platforms and attempt to blast you off the platform the second you land. Through trial and error, the player must figure out ways to defend themselves and/or fight back.
+                <br></br><br></br>
+                <em>Escape</em> is the first playable game I ever successfully coded, and, in fact, is the project that ultimately pushed me into a formal study of programming logic. Primarily a personal learning exercise, I single-handedly took on almost every aspect of the game myself, including (but not limited to): level design, sound desing, character animations, cutscene animations, and 3D modeling. I even did all the voice acting!
+            </p>
         </div>
     )
 }
--- pages/prodrome.js
+++ pages/prodrome.js
@@ -22,7 +22,7 @@ export default function Prodrome() {
             <p>
                 <b>Prodrome</b> is a first-person horror/puzzle game currently in the final stages of development. The player plays as a nurse in a small mountain town where a mysterious portal has appeared and trapped the townspeople in a time loop. Once a person enters the portal, they are transported back to the place and time at which they first laid eyes on the portal. Once they have gone through the portal, they slowly begin to deteriorate and can only be restored by going back through the portal, thus becoming trapped.
                 <br></br><br></br>
-                The player is tasked with cracking the portal's mysteries so they can return home to their wife without deteriorating in the process. Due to the portal's effects (which manisfest through gradual blurring of vision and slowness of movement), the player can only venture so far away from the portal before crossing a threshold at which they will never be able to return to it, and will thus wither away and die. 
+                The player is tasked with cracking the portal's mysteries so they can return home to their wife without deteriorating in the process. Due to the portal's effects (which manisfest through gradual blurring of vision and slowness of movement), the player can only venture so far away from the portal before crossing a threshold at which they will never be able to return to it, and will thus wither away and die. So time management is key!
             </p>
         </div>
     )
