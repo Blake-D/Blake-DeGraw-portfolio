3c32414b20067e144bd4d725da2c4be23a020988
Blake DeGraw <bbdegraw@gmail.com>
2021-03-10 22:03:34 -0800

    home page styled pretty well. still no blurb

--- pages/index.js
+++ pages/index.js
@@ -4,22 +4,84 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <div className="page-container-home">
-      <p className="title-text">Blake DeGraw</p>
       <Head>
         <title>Home | Blake DeGraw</title>
       </Head>
-        <>
-          <Link href="/"><a>home</a></Link>{' | '}
-          <Link href="/about"><a>about</a></Link>{' | '}
-          <Link href="/webapps"><a>web apps</a></Link>{' | '}
-          <Link href="/games"><a>unreal projects</a></Link>{' | '}
+        <div className="nav-links">
+          <Link href="/"><a>home | </a></Link>
+          <Link href="/about"><a>about | </a></Link>
+          <Link href="/webapps"><a>web apps | </a></Link>
+          <Link href="/games"><a>unreal projects | </a></Link>
           <Link href="/contact"><a>contact me</a></Link>
-        </>
-        {/* <p className="title-text">Blake DeGraw</p> */}
-        <h4>
-          Have fun perusing my various projects, and please reach out to me if you have any questions.<br/>
-          (The only thing I enjoy more than creating is geeking out over my projects.)
-        </h4>
+        </div>
+        <p className="title-text">Blake DeGraw</p>
+        <div className="wrap">
+          <div className="left">
+            <h3>LANGUAGES</h3>
+            <p>
+              JavaScript<br></br>
+              Python<br></br>
+              HTML<br></br>
+              CSS<br></br>
+              PHP<br></br>
+              Blueprints<br></br>
+              Max/MSP<br></br>
+              SQL
+            </p>
+          </div>
+          <div className="right">
+            <h3>DATABASES</h3>
+            <p>
+              PostgreSQL<br></br>
+              Sequelize<br></br>
+              MongoDB<br></br>
+              Mongoose
+            </p>
+          </div>
+          <div className="center">
+            <h3>FRAMEWORKS</h3>
+            <p>
+              React<br></br>
+              Express<br></br>
+              Node<br></br>
+              Next.js
+            </p>
+          </div>
+        </div>
+        <style jsx>
+          {`
+            .wrap{
+              text-align: center;
+            }
+            .left{
+              margin-left: 20vw;
+              float: left;
+              color: white;
+            }
+            .right{
+              margin-right: 20vw;
+              float: right;
+              color: white;
+            }
+            .center{
+              margin: 0 auto !important;
+              display: inline-block;
+            }
+            a{
+              font-size: 2vw;
+              color: white;
+            }
+            .title-text{
+              color: white;
+              font-size: 20vw;
+              text-align: left;
+              margin-top: 10;
+            }
+            .nav-links{
+              text-align: right;
+            }
+          `}
+        </style>
     </div>
   )
 }
